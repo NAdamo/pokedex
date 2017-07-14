f1df44ae076ce7c4704f59c07af102017a67fdae
NAdamo <adam.nemeth@dpc.hu>
2017-07-14 14:48:12 +0200

    pokemon render added

--- pokemons.js
+++ pokemons.js
@@ -1,3 +1,61 @@
 /**
  * Created by nadamo on 2017. 07. 14..
  */
+var pokemons = [
+	{
+		"id": 1,
+		"name": "Bulbasaur",
+		"type": [ "grass", "poison" ],
+		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+	},
+	{
+		"id": 5,
+		"name": "Charmeleon",
+		"type": [ "fire" ],
+		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
+	},
+	{
+		"id": 15,
+		"name": "Beedrill",
+		"type": [ "bug", "poison" ],
+		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
+	},
+	{
+		"id": 193,
+		"name": "Yanma",
+		"type": [ "bug", "flying" ],
+		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png"
+	},
+	{
+		"id": 152,
+		"name": "Chikorita",
+		"type": [ "grass" ],
+		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"
+	}
+]
+
+
+
+/*
+* <li>
+ <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+ alt="bulbasaur" width="96" height="96">
+ <label>Bulbasaur</label>
+ <span>
+ <span class="grass">grass</span>
+ <span class="poison">poison</span>
+ </span>
+ </li>
+* */
+
+function renderPokemon(){
+	let li = document.createElement('li');
+	let img = document.createElement('img');
+	img.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
+
+	li.appendChild(img);
+	document.querySelector('main ul').appendChild(li);
+	
+}
+
+renderPokemon();
